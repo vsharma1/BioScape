3ec28eebe05c4a5f21b3c5529d16699868feb6a1
Vishakha Sharma <sh_vishakha@yahoo.com>
2014-05-13 04:01:12 -0400

    Remove build

--- source/_build/lexer.mli
+++
@@ -1,6 +0,0 @@
-exception Illegal_character
-type proc = Process.t
-type action = Action.t
-type token = Parser.token
-val parse_file : string -> (float*int*bool*bool* ((Action.t*string) list * (Species.t*string) list)) * (Environment.t * Volume.t * Process.t * string)
-val parse_string : string -> (float*int*bool*bool* ((Action.t*string) list * (Species.t*string) list)) * (Environment.t * Volume.t * Process.t * string)
--- source/_build/utils.mli
+++
@@ -1,4 +0,0 @@
-val common : 'a list -> 'a list -> ('a list * 'a list * 'a list)
-type pos = float*float*float
-val distance_vector : pos -> pos -> float
-val avg_vector : pos -> pos -> pos
